ed493495f5a0ef1e51f38f626524db113d0b9e9a
sanzh <sanzhar285@gmail.com>
2025-05-30 18:17:46 +0500

    added radzen package

--- obj/Debug/net7.0/MoneyRate.AssemblyInfo.cs
+++ obj/Debug/net7.0/MoneyRate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyRate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f838aba0973ea32268753a52c39dd02da464fea")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyRate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyRate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
